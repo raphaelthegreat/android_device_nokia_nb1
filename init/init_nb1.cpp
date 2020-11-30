078e632b245c0724e939bb5c744e59966b4b5750
George <geoster3d@gmail.com>
2020-11-30 14:44:26 +0200

    NB1: Reintroduct vendor init lib
    Set build fingerprint to Sirocco A10 for proper Play Store apps

---
+++ init/init_nb1.cpp
@@ -0,0 +1,74 @@
+/*
+   Copyright (C) 2007, The Android Open Source Project
+   Copyright (c) 2016, The CyanogenMod Project
+   Copyright (c) 2017, The LineageOS Project
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+#include <android-base/strings.h>
+
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include "vendor_init.h"
+#include "property_service.h"
+
+#define DEVINFO_FILE "/sys/project_info/project_name"
+#define SENSOR_VERSION_FILE "/sys/devices/soc/soc:fingerprint_detect/sensor_version"
+
+using android::base::Trim;
+using android::base::ReadFileToString;
+
+void property_override(char const prop[], char const value[], bool add = true)
+{
+    auto pi = (prop_info *) __system_property_find(prop);
+
+    if (pi != nullptr) {
+        __system_property_update(pi, value, strlen(value));
+    } else if (add) {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
+}
+
+void vendor_load_properties() 
+{
+    LOG(INFO) << "Loading vendor specific properties";
+    
+    property_override("ro.build.fingerprint", "Nokia/Avenger_00WW/A1N_sprout:10/QKQ1.190828.002/00WW_5_140:user/release-keys");
+    property_override("ro.system.build.fingerprint", "Nokia/Avenger_00WW/A1N_sprout:10/QKQ1.190828.002/00WW_5_140:user/release-keys");
+    property_override("ro.vendor.build.fingerprint", "Nokia/Avenger_00WW/A1N_sprout:10/QKQ1.190828.002/00WW_5_140:user/release-keys");
+}
