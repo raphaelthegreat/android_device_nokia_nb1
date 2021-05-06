3d3ee6edcd5c92d2e456355ee14746f93397c61a
George <geoster3d@gmail.com>
2021-05-06 17:07:49 +0300

    NB1: Patch the audio HAL to work with R vendor
    * Use the proper name for libtinycompress and load libprocessgroup.so for set_sched_policy symbol

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/hw/audio.primary.msm8998.so|vendor/lib64/hw/audio.primary.msm8998.so)
+            "${PATCHELF}" --add-needed "libprocessgroup.so" "${2}"
+            "${PATCHELF}" --replace-needed "libtinycompress_vendor.so" "libtinycompress.so" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
