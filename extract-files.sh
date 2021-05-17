89451c108ae1c4548f4143c09aa03d904f4fbabc
George <geoster3d@gmail.com>
2021-05-17 22:19:28 +0300

    NB1: Decommonise blob_fixup
    * We shouldn't have this in the common tree.

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,29 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        ## NB1 Patches
+        vendor/lib/hw/audio.primary.msm8998.so|vendor/lib64/hw/audio.primary.msm8998.so)
+            "${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+            ;;
+        # Patch gx_fpd for VNDK support
+        vendor/bin/gx_fpd)
+            "${PATCHELF}" --remove-needed "libunwind.so" "${2}" 
+            "${PATCHELF}" --remove-needed "libbacktrace.so" "${2}"
+            "${PATCHELF}" --add-needed "liblog.so" "${2}"
+            ;;
+        # Hexedit gxfingerprint to load goodix firmware from /vendor/firmware/
+        vendor/lib64/hw/gxfingerprint.default.so)
+            sed -i -e 's|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g' "${2}"
+            ;;
+        # Use vendor version of libgui
+        vendor/lib/hw/camera.msm8998.so)
+            "${PATCHELF}" --replace-needed "libgui.so" "libgui_vendor.so" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
